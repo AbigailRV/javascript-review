477a840220f21df23a7faca1ccad297610327052
AbigailRV <abigail.raymundo.v@gmail.com>
2026-04-16 16:58:17 -0500

    feat(currency): add currency value object.

---
+++ src/shared/domain/model/currency.js
@@ -0,0 +1,11 @@
+export class Currency {
+    static #VALID_CODES = ['USD', 'JPY', 'EUR' , 'GBP'];
+    #code;
+
+    constructor(code) {
+        if (!Currency.#VALID_CODES.includes(code)) {
+            throw new Error(`Invalid currency code: ${code}. Must be one of: ${Currency.#VALID_CODES.join(', ')}`);
+        }
+    }
+
+}
